d9119a934ca30d7549a135631ed0df394d685176
unknown <osamamuhammed555@gmail.com>
2021-12-26 15:35:09 +0300

    final result v.1.0

--- clear_invoice_data.py
+++ clear_invoice_data.py
@@ -1,11 +1,10 @@
 import os
 import pandas as pd
-
+from openpyxl import load_workbook
 
 
 FILE_PATH = "output/"
 FILES = os.listdir(FILE_PATH)
-
 
 
 template_name = "heading_template.xlsx"
@@ -33,7 +32,6 @@ for excel_file in FILES:
 
                     # ! write code here
                     # * we passed the condition here
-                    pass
                 elif value == 1:
                     # ! write code here
                     # ? loop for delete cells in data
@@ -43,7 +41,76 @@ for excel_file in FILES:
                             pass
                         else:
                             main_df.at[i, column] = None
-                elif value == 0:
-                    pass
 
     main_df.to_excel(FILE_PATH + excel_file, header=True, index=False)
+
+    # todo // open file with openpyxl
+    wb_object = load_workbook(FILE_PATH + excel_file)
+
+    # todo // select active sheet
+    sheet_object = wb_object.active
+
+    # todo // add SUM function to the cell
+
+    max_col = sheet_object.max_column
+    max_row = sheet_object.max_row
+    amount_cord = None
+    quantity_cord = None
+    total_quantity_cord = None
+    total_cord = None
+    # Loop will print all columns name
+    for i in range(1, max_col + 1):
+        cell_obj = sheet_object.cell(row=1, column=i)
+        if cell_obj.value == "Amount (Items)":
+            amount_cord = cell_obj.coordinate
+
+        elif cell_obj.value == "Quantity (Items)":
+            quantity_cord = cell_obj.coordinate
+
+        elif cell_obj.value == "Total Quantity":
+            total_quantity_cord = cell_obj.coordinate
+
+        elif cell_obj.value == "Total":
+            total_cord = cell_obj.coordinate
+
+    net_amount_cord = "".join([i for i in amount_cord if not i.isdigit()])
+    start_amount_cord = net_amount_cord + "2"
+    end_amount_cord = net_amount_cord + str(max_row)
+
+    net_quantity_cord = "".join([i for i in quantity_cord if not i.isdigit()])
+    start_quantity_cord = net_quantity_cord + "2"
+    end_quantity_cord = net_quantity_cord + str(max_row)
+
+    #  todo
+    net_total_quantity_cord = "".join(
+        [i for i in total_quantity_cord if not i.isdigit()]
+    )
+    start_total_quantity_cord = net_total_quantity_cord + "2"
+    end_total_quantity_cord = net_total_quantity_cord + str(max_row)
+    #  todo
+
+    net_total_cord = "".join([i for i in total_cord if not i.isdigit()])
+    start_total_cord = net_total_cord + "2"
+    end_total_cord = net_total_cord + str(max_row)
+
+    # sheet_object[start_total_quantity_cord] = f"=SUM({start_quantity_cord}:{end_quantity_cord})"
+    # sheet_object[start_total_cord] = f"=SUM({start_amount_cord}:{end_amount_cord})"
+
+    amount_data = [net_amount_cord + str(i + 1) for i in range(1, max_row + 1)]
+    quantity_data = [net_quantity_cord + str(i + 1) for i in range(1, max_row + 1)]
+    total = 0
+    total_quantity = 0
+    amount_data.pop()
+    quantity_data.pop()
+
+    for amount_value, total_quantity_value in zip(amount_data, quantity_data):
+
+        total += sheet_object[amount_value].value
+        total_quantity += sheet_object[total_quantity_value].value
+
+    sheet_object[start_total_quantity_cord] = total_quantity
+    sheet_object[start_total_cord] = total
+
+    # todo // close the sheet & file
+
+    wb_object.save(FILE_PATH + excel_file)
--- concat_excel_files.py
+++ concat_excel_files.py
@@ -3,18 +3,21 @@ import pandas as pd
 
 
 FILE_PATH = "output/"
-OUT_FILE_PATH = 'out/'
+OUT_FILE_PATH = "out/"
 FILES = os.listdir(FILE_PATH)
 
-main_df = pd.read_excel("heads.xlsx", header=0)
+# main_df = pd.read_excel("heads.xlsx", header=0)
+main_df = pd.DataFrame()
 
 
 for excel_file in FILES:
 
-    second_df = pd.read_excel(FILE_PATH + excel_file, header=0)
+    second_df = pd.read_excel(FILE_PATH + excel_file)
+
     main_df = pd.concat([main_df, second_df], axis=0)
+
     deleted_file = "output\\" + f"{excel_file}"
 
     os.system(f"del {deleted_file}")
 
-main_df.to_excel(OUT_FILE_PATH + 'final_exported_data.xlsx', header=True, index=False)
+main_df.to_excel(OUT_FILE_PATH + "final_exported_data.xlsx", header=True, index=False)
